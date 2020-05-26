129857ad38445fedab5a984c60b391443dd3cd94
Tiago Silva <tisilva.developer@gmail.com>
2020-05-26 12:45:54 -0300

    chore: Commit Inicial - Calculos com JS

---
+++ IntroducaoWEB/aulajs/index.js
@@ -0,0 +1,13 @@
+// Criar programa que calcula media das notas 
+// entre alunos e envia mensagem do calculo da media
+
+const aluno1 = 'Ze'
+const aluno2 = 'Dubai'
+const aluno3 = 'Dab'
+const notaAluno1 = 9.2
+const notaAluno2 = 3.1
+const notaaluno3 = 5.3
+
+const media = (notaAluno1 + notaAluno2 + notaaluno3) / 3
+
+console.log(media)
