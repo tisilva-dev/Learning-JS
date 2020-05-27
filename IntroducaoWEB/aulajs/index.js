8022f91ed1cfd5a182194bb873bbd0073c90fc03
Tiago Silva <tisilva.developer@gmail.com>
2020-05-27 08:26:15 -0300

    chore: Condicionais

--- IntroducaoWEB/aulajs/index.js
+++ IntroducaoWEB/aulajs/index.js
@@ -1,13 +1,21 @@
 // Criar programa que calcula media das notas 
 // entre alunos e envia mensagem do calculo da media
 
+
 const aluno1 = 'Ze'
-const aluno2 = 'Dubai'
-const aluno3 = 'Dab'
 const notaAluno1 = 9.2
+
+const aluno2 = 'Dubai'
 const notaAluno2 = 3.1
+
+const aluno3 = 'Dab'
 const notaaluno3 = 5.3
 
 const media = (notaAluno1 + notaAluno2 + notaaluno3) / 3
 
-console.log(media)
+// Se a media for maior que 5, mostrar uma mensagem
+if(media > 5){
+    console.log(`A nota foi ${media}. Parabéns.`)
+}else{
+    console.log(`A média ${media} é menor que 5`)
+}
