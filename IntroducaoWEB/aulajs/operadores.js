6946a474924418f6efa9132a33e79046d030f349
Tiago Silva <tisilva.developer@gmail.com>
2020-05-27 11:13:09 -0300

    chore: Aprendendo operadores

---
+++ IntroducaoWEB/aulajs/operadores.js
@@ -0,0 +1,81 @@
+/*
+
+OPERADORES
+
+>   Maior
+<   Menor
+>=  Maior Igual
+<=  Menor Igua
+==  Igual
+=== Igual e mesmo tipo
+!=  Diferente de
+!== Diferente, inclusive do tipo
+
+*/
+
+// console.log(5 > 4)
+// console.log(5 < 4)
+// console.log(5 >= 4)
+// console.log(4 <= 4)
+// console.log(4 == "4")
+// console.log(4 === "4")
+// console.log(4 != "5")
+// console.log(4 !== "5")
+
+
+// const idade = 17
+
+// if (idade >= 18) {
+//     console.log("Pode entrar")
+// } else {
+//     console.log("Não pode entrar")
+// }
+
+// if (idade === 17) {
+//     console.log("Volte quando tiver 18 anos")
+// }
+
+/*
+OPERADORES LOGICOS
+
+&& - As duas condições devem ser verdadeiras para que a condição final seja verdadeira.
+|| - Uma das condições deve ser verdadeira para que a condição final seja verdadeira.
+! - Nega uma condição
+*/
+
+// console.log(5 == 5 && 6 == 6)
+// console.log(5 == 5 && 6 != 6)
+
+// console.log(5 != 5 || 6 == 6)
+// console.log(5 == 5 || 6 != 6)
+
+// console.log(!(5 > 6))
+// console.log(!(5 < 6))
+
+// DESAFIO 1
+
+// const idade = 16
+// // verificar se a pessoa é maior ou tem 18 anos
+// // se sim deixar entra, senao, nao deixar entrar
+// if (!(idade >= 18) || idade === 17) {
+//     console.log("Não pode entrar")
+// } else {
+//     console.log("Pode entrar")
+// }
+
+/*
+OPERADORES ARITMETIVOS
+
++   Adicao
+-   Subtracao
+*   Multiplição
+/   Divisão
+%   Resto da divisão
+
+*/
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 % 1.5)
