c050802d8373d3214ba1e8ce27541bc1e3cbb87e
Tiago Silva <tisilva.developer@gmail.com>
2020-12-30 12:10:30 -0300

    Adicionando arquivos de comandos básicos

---
+++ Comandos vscode.js
@@ -0,0 +1,20 @@
+// Comandos vscode
+
+// Shift+Alt+F = Format Document
+// ctrl+shift+p = paleta de comandos
+// ctrl + shift + n = executar codigo com runner
+// ctrl + ' = abrir terminal 
+// ctrl + shift + u = abrir output
+// ctrl + ; = comentar vária linhas
+// alt = click = selecionar diversas linhas
+// ctrl + d = selecionar proximas correspondentes
+// ctrl+shift+L = selecionar todas as correpondencias da atual seleção
+// ctrl+l = seleciona a linha atual
+// ctrl+h = replace
+// alt+up or alt+down = move linha para cima ou baixo
+// ctrl+b = minimize sidebar
+// ctrl+tab or ctrl+shift+Tab = change tab
+// ctrl+r = change folders(workspace)
+// ctrl+ g = go to line
+// ctrl+p = go to file
+// ctrl+o = open file
